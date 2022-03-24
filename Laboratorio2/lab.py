0ac337a02f0d301060aa62bb2464749b99e3859d
PabloCorrea99 <pcorream2@eafit.edu.co>
2022-03-23 22:38:11 -0500

    Cambio en directorio

--- Laboratorio2/lab.py
+++ Laboratorio2/lab.py
@@ -1,11 +1,14 @@
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 discos = r"\discos.txt"
 board = r"\board.txt"
 mac = r"\AMAC.txt"
 ipInfo = r"\AipInfo.txt"
 biosInfo = r"\biosInfo.txt"
 cpuInfo = r"\ACPUInfo.txt"
-directorio = r"C:\Users\Usuario\Desktop\Universidad\9SEMESTRE\OPERATIVOS\OperativeSystems\Laboratorio2"
+directorio = str(BASE_DIR)
 os.system("wmic diskdrive get systemName,model,index,firmwareRevision,status,interfaceType,totalHeads,totalTracks,totalCylinders,totalSectors,partitions > "+directorio+discos)
 os.system("wmic baseboard get /All > "+directorio+board)
 os.system("getmac > "+directorio+mac)
